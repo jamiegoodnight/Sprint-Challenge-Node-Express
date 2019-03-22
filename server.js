bbf8b794ced6306414c11f5c3e95453d30df05e0
Jamie Goodnight <goodnightlambda@gmail.com>
2019-03-22 11:18:28 -0400

    Added dependencies. Added boilerplate.

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const projectsRouter = require("./projects/projects-router.js");
+const actionsRouter = require("./actions/actions-router.js");
+
+const server = express();
+
+server.use(express.json());
+server.use("/api/projects", projectsRouter);
+server.use("/api/actions", actionsRouter);
+
+module.exports = server;
