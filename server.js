17e57608b34eaf58c6a2ed25d2f4994c217a3454
Jamie Goodnight <goodnightlambda@gmail.com>
2019-03-22 14:00:19 -0400

    Added middleware for length check

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ server.use(express.json());
 server.use("/api/projects", projectsRouter);
 server.use("/api/actions", actionsRouter);
 
+const lengthMax = (req, res, next) => {
+  if (req.body.name.length > 128) {
+    res
+      .status(404)
+      .json({ message: "this name must be less than 128 characters" });
+    next();
+  } else {
+    next();
+  }
+};
+
+server.use(lengthMax);
+
 server.get("/", (req, res) => {
   res.send(console.log("server restart"));
 });
